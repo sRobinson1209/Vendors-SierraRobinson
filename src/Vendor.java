aaa8cc7d0313ec464ba7a7e0cd66df7f8c73559a
sRobinson1209 <150472871+sRobinson1209@users.noreply.github.com>
2024-11-20 15:01:41 -0600

    Created the method for changing the name of an Item, and created the test to make sure the Item name is changed. The test did indeed pass!

--- src/Vendor.java
+++ src/Vendor.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.SysexMessage;
 import java.util.HashMap;
 
 
@@ -6,8 +7,9 @@ import java.util.HashMap;
  * well as the current balance of money that has been deposited into the machine.
  */
 class Vendor {
-    private static HashMap<String, Item> Stock = new HashMap<String,Item>();
+    public static HashMap<String, Item> Stock = new HashMap<String, Item>();
     private double balance;
+    private HashMap<String, Integer> purchaseHistory = new HashMap<>();
 
     Vendor(int numCandy, int numGum) {
         Stock.put("Candy", new Item(1.25, numCandy));
@@ -15,43 +17,73 @@ class Vendor {
         this.balance = 0;
     }
 
-
-
-    /** resets the Balance to 0 */
-    void resetBalance () {
+    /**
+     * resets the Balance to 0
+     */
+    void resetBalance() {
         this.balance = 0;
     }
 
-    /** returns the current balance */
-    double getBalance () {
+    /**
+     * returns the current balance
+     */
+    double getBalance() {
         return this.balance;
     }
 
-    /** adds money to the machine's balance
+    /**
+     * adds money to the machine's balance
+     *
      * @param amt how much money to add
-     * */
-    void addMoney (double amt) {
+     */
+    void addMoney(double amt) {
         this.balance = this.balance + amt;
     }
 
-    /** attempt to purchase named item.  Message returned if
+    /**
+     * attempt to purchase named item.  Message returned if
      * the balance isn't sufficient to cover the item cost.
      *
      * @param name The name of the item to purchase ("Candy" or "Gum")
      */
-    void select (String name) {
+    void select(String name) {
         if (Stock.containsKey(name)) {
             Item item = Stock.get(name);
             if (balance >= item.price) {
-                item.purchase(1);
-                this.balance = this.balance - item.price;
-            }
-            else
-                System.out.println("Gimme more money");
-        }
-        else System.out.println("Sorry, don't know that item");
-    }
+                if (item.stock > 0) { // Assuming getStock() checks item quantity
+                    item.purchase(1);
+                    this.balance = this.balance - item.price;
 
+                    purchaseHistory.put(name, purchaseHistory.getOrDefault(name, 0) + 1);
+
+                    System.out.println("Purchase Successful");
+                } else {
+                    System.out.println(name + " is out of stock.");
+                }
+            } else {
+                System.out.println("Gimme more money");
+            }
+        } else {
+            System.out.println("Sorry, don't know that item.");
+        }
+    }
+    void restock(String name, int amount, double price){
+        if(Stock.containsKey(name)){
+            Stock.get(name).restock(amount);
+        } else {
+            Stock.put(name, new Item(price, amount));
+        }
+        System.out.println(name + "Restocked with " + amount + "Pieces");
+    }
+    void changeItemName(String oldName, String newName){
+        if(Stock.containsKey(oldName)){
+            Item item = Stock.remove(oldName);
+            Stock.put(newName, item);
+            System.out.println("Item renamed to " + newName);
+        } else {
+            System.out.println("Item "+ oldName +"Not found");
+        }
+    }
 }
 
 class Examples {
--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -1,7 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class VendorTest {
     static Vendor vendor;
@@ -19,9 +20,32 @@ public class VendorTest {
         vendor.addMoney(10);
         assertEquals(10, vendor.getBalance(), 0.001);
     }
+//    @Test
+//    public void buyItemTest(){
+//        vendor.addMoney(1.50);
+//        vendor.select("Candy");
+//        //assertEquals(1,1);
+//        assertEquals(1.25, vendor.Stock.get("Candy").stock);
+//        assertEquals(0.25, vendor.getBalance());
+//    }
+   // @Test
+//    void testRestockNewItem() {
+//        // Arrange
+//        Item inventory = new Item(); // Replace with the actual class name
+//
+//        // Act
+//        inventory.restock(2,1, 1.25);
+//
+//        // Assert
+//        //assertTrue(inventory.Stock.containsKey("Candy"));
+//        assertEquals(20, inventory.Stock.get("Candy").getStock());
+//        assertEquals(0.5, inventory.Stock.get("Candy").getPrice(), 0.001);
+//    }
     @Test
-    public void buyItemTest(){
-
+    void changeItemNameTest(){
+        vendor.changeItemName("Candy", "Twix Bar");
+        Assertions.assertTrue(vendor.Stock.containsKey("Twix Bar"));
+        Assertions.assertFalse(vendor.Stock.containsKey("Candy"));
     }
 
 }
